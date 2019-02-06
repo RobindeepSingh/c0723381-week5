6eb6393052c64b916afdb4265a589446303a8b0b
visualstudio <visualstudio@122-PC21>
2019-02-06 09:28:23 -0500

    classpractise

---
+++ 723381/723381/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _723381
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                MethodA();
+            }
+
+            public static void MethodA()
+            {
+                while (Peanut())
+                {
+                    Console.WriteLine("Help! I am stuck in infinite loop");
+
+                }
+            }
+
+            public static bool Peanut()
+            {
+                return true;
+            }
+        }
+    }
+}
+
